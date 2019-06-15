82c16c9b21c6fc3326627b1dcce0efe4884bc36b
YcodeMan <1130578809@qq.com>
2019-06-15 17:31:54 +0800

    test

--- src/store/reducers/List/index.js
+++ src/store/reducers/List/index.js
@@ -1,9 +1,25 @@
 
 
 const defaultState = {
-    list: ['ls']
+    val: '',
+    list: []
 }
 
 export default (state=defaultState, action) => {
+    switch(action.type) {
+        case 'VALBIND':
+            let valBindState = JSON.parse(JSON.stringify(state))
+            valBindState.val = action.val
+            return valBindState
+        case 'VAL_TO_ARR':
+            let arrState = JSON.parse(JSON.stringify(state))
+            arrState.list.push(arrState.val)
+            arrState.val = ''
+            return arrState
+        case 'DEL_VAL_ARR':
+            let newState = JSON.parse(JSON.stringify(state))
+            newState.list.splice(action.val, 1) 
+            return newState      
+    }
     return state
 }
