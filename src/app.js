d25bcc2ed6f1109d49c47874d8269157394c4716
YcodeMan <1130578809@qq.com>
2019-06-15 15:54:07 +0800

    测试文件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import React,{Component} from 'react'
-
+import store from '@store'
 export default class App extends Component{
+    constructor() {
+        super()
+        this.state = store.getState()
+       store.subscribe(this.updateView.bind(this))
+    }
     render() {
+        let {n} = this.state
         return(
-            <div>app</div>
+            <div>
+                {n}<br/>
+                <button onClick={this.addNum.bind(this)}>自增</button>
+            </div>
         )
     }
+    addNum() {
+        let action = {
+            type: 'ADD_NUM'
+        }
+        store.dispatch(action)
+    }
+
+    // 更新视图
+    updateView() {  
+        this.setState(store.getState())
+    }
+
 }
